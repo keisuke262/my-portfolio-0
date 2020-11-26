d74b04f08904a9f7f4a97a2250de5e369da21c67
keisuke <keikei1166guiyou@gmail.com>
2020-11-26 09:07:59 +0900

    set preparation

---
+++ config/initializers/generators.rb
@@ -0,0 +1,9 @@
+#CSS, Javascript, Helper関係のファイルが自動生成されない
+#Routerにルーティングを自動的に追加することもない
+
+Rails.application.config.generators do |g|
+    g.stylesheet false
+    g.javascripts false
+    g.helper false
+    g.skip_routes true
+end
