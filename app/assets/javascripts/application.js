f196d77700e17383afbd965157a49500577d9def
keisuke <keikei1166guiyou@gmail.com>
2020-11-29 18:08:04 +0900

    carousel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,10 +16,45 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-$(function() {
-    $('.slider').slick({
-        //dots: true,
-        autoplay: true,
-        adaptiveHeight: true
+
+$(document).on('turbolinks:load', function(){
+    $('.carousel').slick({
+      autoplay:true, 
+      dots:true,  
+      prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+      nextArrow: '<button class="slide-arrow next-arrow"></button>',
+      
     });
-});
+  });
+
+  $(document).on('turbolinks:load', function(){
+    $('.multiple').slick({
+      autoplay:true,
+      dots:true,     
+      autoplaySpeed: 4000,
+    　slidesToShow: 3, //表示するスライドの数
+    　slidesToScroll: 3, //スクロールで切り替わるスライドの数
+      prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+      nextArrow: '<button class="slide-arrow next-arrow"></button>',
+    　 //レスポンシブ対応させたい場合は以下も記述
+    　 responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          }
+        },
+
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+  });
+
+
